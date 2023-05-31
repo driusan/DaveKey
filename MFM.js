adc600a0e24c7cfe36d7b876bc3f757ad0eabde0
Dave MacFarlane <driusan@mcin.ca>
2023-05-30 22:08:02 -0400

    Many animations implemented
    The keyframes for the CSS animations were taken from calckey. Since we're embedded in a <WebView> we just use the same CSS.

--- MFM.js
+++ MFM.js
@@ -79,6 +79,41 @@ function applyMFMfunc(callback, node) {
         return '<span style="font-size: 600%">' + content + '</span>';
     case 'blur':
         return '<span onclick="this.style.filter = (this.style.filter == \'none\' ? \'blur(6px)\' : \'none\')" style="filter: blur(6px); transition: filter .3s">' + content + '</span>';
+    case 'jelly': {
+        const speed = (node.props.args.speed || "1s")
+        return '<span style="display: inline-block; animation: ' + speed + ' linear 0s infinite normal both running mfm-rubberBand;">' + content + '</span>';
+    }
+    case 'tada': {
+        const speed = (node.props.args.speed || "1s")
+        return '<span style="display: inline-block; font-size: 150%; animation: ' + speed + ' linear 0s infinite normal both running tada;">' + content + '</span>';
+    }
+    case 'jump': {
+        const speed = (node.props.args.speed || "0.75s")
+        return '<span style="display: inline-block; animation: ' + speed + ' linear 0s infinite normal both running mfm-jump;">' + content + '</span>';
+    }
+    case 'bounce': {
+        const speed = (node.props.args.speed || "0.75s")
+        return '<span style="display: inline-block; transform-origin: center bottom; animation: ' + speed + ' linear 0s infinite normal both running mfm-bounce;">' + content + '</span>';
+    }
+    case 'shake': {
+        const speed = (node.props.args.speed || "0.5s")
+        return '<span style="display: inline-block; transform-origin: center bottom; animation: ' + speed + ' ease 0s infinite normal none running mfm-shake;">' + content + '</span>';
+    }
+    case 'twitch': {
+        const speed = (node.props.args.speed || "0.5s")
+        return '<span style="display: inline-block; transform-origin: center bottom; animation: ' + speed + ' ease 0s infinite normal none running mfm-twitch;">' + content + '</span>';
+    }
+    case 'rainbow': {
+        const speed = (node.props.args.speed || "1s")
+        // without a colour hue-rotate won't work for some reason. red is arbitrary.
+        return '<span style="display: inline-block; color: red; animation: ' + speed + ' linear 0s infinite normal none running mfm-rainbow;">' + content + '</span>';
+    }
+    case 'fade': {
+        const speed = (node.props.args.speed || "1.5s")
+        const direction = node.props.args.out ? 'alternate-reverse' : 'alternate';
+        return '<span style="display: inline-block; animation: ' + speed + ' linear 0s infinite ' + direction + ' none running mfm-fade;">' + content + '</span>';
+
+    }
     default:
         console.warn('unhandled fn ' + node.props.name);
         return '<span>' + content + '</span>';
@@ -144,7 +179,230 @@ function MFM2HTML(mfmTree, emojis) {
     // when the object being mapped on is the node and we can't adjust the calling parameters
     nodeClosure.emojis = emojis;
     const nodesAsHTML = mfmTree.map(nodeClosure());
-    return '<body onclick="window.ReactNativeWebView.postMessage(JSON.stringify({type: \'defaultclick\'})); return false;"}><div>' + nodesAsHTML.join('') + "</div></body>";
+    const animations = `<style>
+        @keyframes mfm-twitch {
+                0% {
+                        transform: translate(7px, -2px);
+                }
+                5% {
+                        transform: translate(-3px, 1px);
+                }
+                10% {
+                        transform: translate(-7px, -1px);
+                }
+                15% {
+                        transform: translateY(-1px);
+                }
+                20% {
+                        transform: translate(-8px, 6px);
+                }
+                25% {
+                        transform: translate(-4px, -3px);
+                }
+                30% {
+                        transform: translate(-4px, -6px);
+                }
+                35% {
+                        transform: translate(-8px, -8px);
+                }
+                40% {
+                        transform: translate(4px, 6px);
+                }
+                45% {
+                        transform: translate(-3px, 1px);
+                }
+                50% {
+                        transform: translate(2px, -10px);
+                }
+                55% {
+                        transform: translate(-7px);
+                }
+                60% {
+                        transform: translate(-2px, 4px);
+                }
+                65% {
+                        transform: translate(3px, -8px);
+                }
+                70% {
+                        transform: translate(6px, 7px);
+                }
+                75% {
+                        transform: translate(-7px, -2px);
+                }
+                80% {
+                        transform: translate(-7px, -8px);
+                }
+                85% {
+                        transform: translate(9px, 3px);
+                }
+                90% {
+                        transform: translate(-3px, -2px);
+                }
+                95% {
+                        transform: translate(-10px, 2px);
+                }
+                to {
+                        transform: translate(-2px, -6px);
+                }
+        }
+        @keyframes mfm-rubberBand {
+                0% {
+                        transform: scaleZ(1);
+                }
+                30% {
+                        transform: scale3d(1.25, .75, 1);
+                }
+                40% {
+                        transform: scale3d(.75, 1.25, 1);
+                }
+                50% {
+                        transform: scale3d(1.15, .85, 1);
+                }
+                65% {
+                        transform: scale3d(.95, 1.05, 1);
+                }
+                75% {
+                        transform: scale3d(1.05, .95, 1);
+                }
+                to {
+                        transform: scaleZ(1);
+                }
+        }
+        @keyframes tada {
+                0% {
+                        transform: scaleZ(1);
+                }
+                10%, 20% {
+                        transform: scale3d(.9, .9, .9) rotate3d(0, 0, 1, -3deg);
+                }
+                30%, 50%, 70%, 90% {
+                        transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg);
+                }
+                40%, 60%, 80% {
+                        transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg);
+                }
+                to {
+                        transform: scaleZ(1);
+                }
+        }
+        @keyframes mfm-jump {
+                0% {
+                        transform: translateY(0);
+                }
+                25% {
+                        transform: translateY(-16px);
+                }
+                50% {
+                        transform: translateY(0);
+                }
+                75% {
+                        transform: translateY(-8px);
+                }
+                to {
+                        transform: translateY(0);
+                }
+        }
+        @keyframes mfm-bounce {
+                0% {
+                        transform: translateY(0) scale(1);
+                }
+                25% {
+                        transform: translateY(-16px) scale(1);
+                }
+                50% {
+                        transform: translateY(0) scale(1);
+                }
+                75% {
+                        transform: translateY(0) scale(1.5, .75);
+                }
+                to {
+                        transform: translateY(0) scale(1);
+                }
+        }
+        @keyframes mfm-shake {
+                0% {
+                        transform: translate(-3px, -1px) rotate(-8deg);
+                }
+                5% {
+                        transform: translateY(-1px) rotate(-10deg);
+                }
+                10% {
+                        transform: translate(1px, -3px) rotate(0);
+                }
+                15% {
+                        transform: translate(1px, 1px) rotate(11deg);
+                }
+                20% {
+                        transform: translate(-2px, 1px) rotate(1deg);
+                }
+                25% {
+                        transform: translate(-1px, -2px) rotate(-2deg);
+                }
+                30% {
+                        transform: translate(-1px, 2px) rotate(-3deg);
+                }
+                35% {
+                        transform: translate(2px, 1px) rotate(6deg);
+                }
+                40% {
+                        transform: translate(-2px, -3px) rotate(-9deg);
+                }
+                45% {
+                        transform: translateY(-1px) rotate(-12deg);
+                }
+                50% {
+                        transform: translate(1px, 2px) rotate(10deg);
+                }
+                55% {
+                        transform: translateY(-3px) rotate(8deg);
+                }
+                60% {
+                        transform: translate(1px, -1px) rotate(8deg);
+                }
+                65% {
+                        transform: translateY(-1px) rotate(-7deg);
+                }
+                70% {
+                        transform: translate(-1px, -3px) rotate(6deg);
+                }
+                75% {
+                        transform: translateY(-2px) rotate(4deg);
+                }
+                80% {
+                        transform: translate(-2px, -1px) rotate(3deg);
+                }
+                85% {
+                        transform: translate(1px, -3px) rotate(-10deg);
+                }
+                90% {
+                        transform: translate(1px) rotate(3deg);
+                }
+                95% {
+                        transform: translate(-2px) rotate(-3deg);
+                }
+                to {
+                        transform: translate(2px, 1px) rotate(2deg);
+                }
+        }
+        @keyframes mfm-rainbow {
+                0% {
+                    filter: hue-rotate(0deg) contrast(150%) saturate(150%);
+                }
+                to {
+                    filter: hue-rotate(360deg) contrast(150%) saturate(150%);
+                }
+        }
+        @keyframes mfm-fade {
+                0% {
+                        opacity: 0;
+                }
+                to {
+                        opacity: 1;
+                }
+        }
+    </style>
+    `;
+    return '<body onclick="window.ReactNativeWebView.postMessage(JSON.stringify({type: \'defaultclick\'})); return false;"}>' + animations + '<div>' + nodesAsHTML.join('') + "</div></body>";
 }
 
 const MemoWebView = memo(function MemoWebView(props) {
